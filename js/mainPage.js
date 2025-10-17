b9625e2bfc3cf1a4d3b0d1d93376173e0ec4eaf8
_FN10_ <mathdudex09@gmail.com>
2025-10-16 21:19:09 -0300

    init, moved, and switched to hcapcha

---
+++ js/mainPage.js
@@ -0,0 +1,82 @@
+//@ts-check
+
+const statusText = document.getElementById("status-text");
+const chatsDisplay = document.getElementById("chatsDisplay");
+const signupCaptcha = document.getElementById("signupCaptcha");
+
+if (signupCaptcha != null) {
+  signupCaptcha.addEventListener("verified", (e) => {
+    // @ts-ignore
+    console.log("verified event", { token: e.token });
+    // @ts-ignore
+    console.log("Local key: " + e.token);
+    fetch("https://mpc.xplate.dev/api/capchaValid", {
+      method: "POST",
+      // @ts-ignore
+      body: e.token,
+      headers: {
+        "Content-type": "text/plain; charset=UTF-8",
+      },
+    }).then((res) => {
+      console.log(res);
+    });
+  });
+  signupCaptcha.addEventListener("error", (e) => {
+    console.log("error event", { error: e.error });
+  });
+}
+
+fetch("https://mpc.xplate.dev/api/getRoomsAndPeopleInThem").then((res) => {
+  if (res.ok) {
+    res.json().then((data) => {
+      //const parsed = JSON.parse(data);
+      let peopleOn = 0;
+      let chatsOpen = 0;
+      for (const key in data) {
+        peopleOn += data[key].peopleCurrently;
+        chatsOpen++;
+        const chatVisualHTML = `
+		<div
+          style="
+            min-height: 4rem;
+            background-color: white;
+            border: 0.2rem double black;
+			margin-bottom: 0.6rem;
+          "
+        >
+          <p
+            style="
+              margin: -3px 0;
+              -webkit-text-fill-color: white;
+              -webkit-text-stroke: 1px;
+              user-select: none;
+            "
+            class="bbh-sans-bartle-regular"
+          >
+            ${data[key].name}
+          </p>
+          <img
+            src="people.png"
+            style="image-rendering: crisp-edges; padding-left: 4px; margin-top: 4px;"
+            width="20px"
+          />
+          <i
+            class="noto-sans"
+            style="-webkit-text-stroke: 0; -webkit-text-fill-color: black"
+            ><sup>${data[key].peopleCurrently}/${data[key].maxPeople}</sup></i
+          >
+          <div style="display: flex; padding: 5px; justify-content: right">
+            <button>Join!</button>
+          </div>
+        </div>`;
+        chatsDisplay?.insertAdjacentHTML("beforeend", chatVisualHTML);
+        console.log(
+          `chat with id: ${key}, has ${data[key].peopleCurrently}/${data[key].maxPeople} people in it`
+        );
+      }
+      if (statusText != null)
+        statusText.innerHTML = `<i>${peopleOn} people are online in ${chatsOpen} chats.</i>`;
+      else console.error("No status text!");
+    });
+  }
+});
